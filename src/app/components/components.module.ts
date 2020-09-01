4ded8dab3f39326300dae98f095b2a1291fb526b
Raphael Vazquez <rvazquez@ids.com.mx>
2020-09-01 11:11:48 -0500

    feat: adding components

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [HeaderComponent, SidebarComponent],
+  exports: [HeaderComponent, SidebarComponent],
+})
+export class ComponentsModule {}
